641a140437afadd001f56fd675beee3f1103e6fc
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-11-26 12:44:37 -0300

    fix: validation url

--- dist/controller/register/validate/validateRegister.js
+++ dist/controller/register/validate/validateRegister.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyRegisterUser = void 0;
 const verifyRegisterUser = (data) => {
-    const urlRegex = /^(?:https?:\/\/)?[^\s\/]+(\.[^\s\/]+)*$/;
+    // const urlRegex = /^(?:https?:\/\/)?[^\s\/]+(\.[^\s\/]+)*$/
     switch (true) {
         case data.typeUser.toUpperCase() !== "CLIENT" && data.typeUser.toUpperCase() !== "DIARIST":
             return "Type user is invalid";
         case typeof data.name !== "string" || data.name === "" || data.name.length < 2:
             return "Invalid name";
-        case !urlRegex.test(data.photoUrl):
-            return "Invalid URL";
+        // case !urlRegex.test(data.photoUrl):
+        // return "Invalid URL";
         case typeof data.userMysqlId !== "number":
             return "Invalid the id user, it must be a number";
         default:
--- src/controller/register/validate/validateRegister.ts
+++ src/controller/register/validate/validateRegister.ts
@@ -2,15 +2,15 @@ import { RegisterUser } from "../interface/interfaceRegister";
 
 const verifyRegisterUser = (data: RegisterUser) => {
 
-  const urlRegex = /^(?:https?:\/\/)?[^\s\/]+(\.[^\s\/]+)*$/
+  // const urlRegex = /^(?:https?:\/\/)?[^\s\/]+(\.[^\s\/]+)*$/
 
   switch (true) {
     case data.typeUser.toUpperCase() !== "CLIENT" && data.typeUser.toUpperCase() !== "DIARIST":
       return "Type user is invalid";
     case typeof data.name !== "string" || data.name === "" || data.name.length < 2:
       return "Invalid name";
-    case !urlRegex.test(data.photoUrl):
-      return "Invalid URL";
+    // case !urlRegex.test(data.photoUrl):
+      // return "Invalid URL";
     case typeof data.userMysqlId !== "number":
       return "Invalid the id user, it must be a number";
     default:
